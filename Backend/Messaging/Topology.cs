8f3499297317f09a8f0380123bad0af16c59169f
bo.yang@rewe-group.at <bo@MacBook-Air-von-Bo.local>
2025-11-05 13:55:11 +0100

    added rabbitmq in the infrastructure

---
+++ Backend/Messaging/Topology.cs
@@ -0,0 +1,14 @@
+namespace Swen3.API.Messaging
+{
+    public static class Topology
+    {
+        public const string Exchange = "documents";
+        public const string RoutingKey = "document.uploaded";
+        public const string Queue = "documents.ocr";
+
+        public const string DeadLetterExchange = "documents.dlx";
+        public const string DeadLetterQueue = "documents.ocr.dlq";
+    }
+}
+
+
